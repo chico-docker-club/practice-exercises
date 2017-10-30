0fb3d503ad0ea88f61b67808105169967446c626
matthewpdias <matthewpdias@gmail.com>
2017-10-30 10:01:30 -0700

    added support for multiple roodleboodles in one commad

--- entrypoint/oodle.rb
+++ entrypoint/oodle.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/env ruby
 input = ARGV
 
-if input.length == 1
-	puts input[0].gsub(/[aeiouyAEIOUY]/i, 'oodle')
-else
+if input.length < 1
 	puts 'Wrong number of args!'
 end
+
+for word in input
+	print word.gsub(/[aeiouyAEIOUY]/i, 'oodle')
+	print " "
+end
+puts
